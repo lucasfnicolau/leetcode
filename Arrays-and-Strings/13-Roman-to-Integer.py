a7b35c0b3d0fddf5eafb6d3b06d83dd8eefa70b3
Lucas Fernandez Nicolau <lucasfnicolau@protonmail.com>
2024-08-30 21:28:42 -0300

    13 - Roman to Integer

---
+++ Arrays-and-Strings/13-Roman-to-Integer.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/roman-to-integer/submissions/1373691107
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict = {
+            'I': 1,
+            'IV': 4,
+            'V': 5,
+            'IX': 9,
+            'X': 10,
+            'XL': 40,
+            'L': 50,
+            'XC': 90,
+            'C': 100,
+            'CD': 400,
+            'D': 500,
+            'CM': 900,
+            'M': 1000
+        }
+
+        number = 0
+        i = 0
+        while i < len(s):
+            if i+1 < len(s):
+                try:
+                    key = s[i] + s[i+1]
+                    number += dict[key]
+                    i += 2
+                except:
+                    number += dict[s[i]]
+                    i += 1
+            else:
+                number += dict[s[i]]
+                i += 1
+
+        return number
