ddd0ce1cc0e5aba1392c906261da5ad3d8ad11fe
Lucas Fernandez Nicolau <lucasfnicolau@protonmail.com>
2024-08-31 12:39:30 -0300

    Update 14-Longest-Common-Prefix.py

--- Arrays-and-Strings/14-Longest-Common-Prefix.py
+++ Arrays-and-Strings/14-Longest-Common-Prefix.py
@@ -4,15 +4,13 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         firstStr = strs[0]
         prefix = ''
-        
+
         for i in range(len(firstStr)):
-            hasPrefix = True
             for s in strs[1:]:
                 if i >= len(s):
                     return prefix
                 elif s[i] != firstStr[i]:
                     return prefix
-            if hasPrefix:
-                prefix += firstStr[i]
+            prefix += firstStr[i]
 
         return prefix
