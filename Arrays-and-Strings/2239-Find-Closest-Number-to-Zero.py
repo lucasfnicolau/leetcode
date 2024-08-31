a30b6d421fb3f566be022e629f1d16c2daa89164
Lucas Fernandez Nicolau <lucasfnicolau@protonmail.com>
2024-08-30 21:55:55 -0300

    Update 2239-Find-Closest-Number-to-Zero.py

--- Arrays-and-Strings/2239-Find-Closest-Number-to-Zero.py
+++ Arrays-and-Strings/2239-Find-Closest-Number-to-Zero.py
@@ -7,9 +7,9 @@ class Solution:
         negative = sys.maxsize * -1
         positive = sys.maxsize
         for n in nums:
-            if n < 0 and (negative == 0 or n > negative):
+            if n < 0 and n > negative:
                 negative = n
-            elif n > 0 and (positive == 0 or n < positive):
+            elif n > 0 and n < positive:
                 positive = n
             elif n == 0: return n
         
